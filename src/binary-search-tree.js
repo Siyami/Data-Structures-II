122764a06fcde8fa4caf4c590df64bfd083cad77
JASPINDER SING <jaspindersingh83@gmail.com>
2017-11-24 17:03:34 -0800

    Solutions

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,3 +1,21 @@
+class Queue {
+  constructor() {
+      this.storage = [];
+    }
+
+  enqueue(x) {
+      this.storage.push(x);
+    }
+
+  dequeue() {
+      return this.storage.shift();
+    }
+
+  isEmpty() {
+      return this.storage.length === 0;
+    }
+}
+
 // https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
 /* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
@@ -12,27 +30,74 @@ class BinarySearchTree {
   // assigns it to either the left or right subtree,
   // depending on its value
   insert(value) {
-
+    if (this.value === undefined) {
+      this.value = value;
+    } else if (this.value >= value) {
+    if (this.left === null) {
+        this.left = new BinarySearchTree;
+        this.left.value = value;
+      } else {
+        this.left.insert(value);
+      }
+  } else {
+    if (this.right === null) {
+        this.right = new BinarySearchTree;
+        this.right.value = value;
+      } else {
+        this.right.insert(value);
+      }
+  }
   }
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
-
+     if (this.value === target) {
+      return true;
+    } else if (this.value > target) {
+      if (this.left === null)  return false;
+      return this.left.contains(target);
+    } else {
+      if (this.right === null)  return false;
+      return this.right.contains(target);
+    }
   }
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
-  // Applies the given callback to each tree node in the process
+  // Applies the given callback to each tree node in the process. 
+  // Below is solution of preorder BST Call (order is root -> left node -> right node)
   depthFirstForEach(cb) {
-
+    if (!this.value) return;
+    else if (!this.left && !this.right) {
+        cb(this.value);
+      } else if (!this.left) {
+      cb(this.value);
+      this.right.depthFirstForEach(cb);
+    } else if (!this.right) {
+      cb(this.value);
+      this.left.depthFirstForEach(cb);
+    } else {
+      cb(this.value);
+      this.left.depthFirstForEach(cb);
+      this.right.depthFirstForEach(cb);
+    } 
   }
+
   // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
   // at the root node, going down to the root node's children, and iterating
   // through all those nodes first before moving on to the next layer of nodes
   // Applies the given callback to each tree node in the process
   // You'll need the queue-helper file for this. Or could you roll your own queue
   // again. Whatever floats your boat.
+  
   breadthFirstForEach(cb) {
+    if (!this.value) return;
+    else { 
+      const myQueue = new Queue();
+      myQueue.enqueue(this.value);
+      while (!myQueue.isEmpty()) {
 
-  }
+      }
+    }
+  }   
 }
 
 module.exports = BinarySearchTree;
--- src/graph.js
+++ src/graph.js
@@ -40,18 +40,42 @@ class Graph {
   // Optionally accepts an array of other GraphNodes for the new vertex to be connected to
   // Returns the newly-added vertex
   addVertex(value, edges = []) {
-
+    const node = new GraphNode({ value, edges });
+    const vertices = this.vertices;
+    vertices.push(node);
+    for (let i = 0; i < edges.length; i++) {
+      const obj = {};
+      obj.value = edges[i];
+      obj.edges = [value];
+      const edgenode = new GraphNode(obj);
+      vertices.push(edgenode);
+    }
+    const verticeslen = this.vertices.length;
+    if (verticeslen === 2 && !vertices[0].edges.length) {        
+      vertices[0].edges = vertices[1];
+      vertices[1].edges = vertices[0];
+    }
   }
   // Checks all the vertices of the graph for the target value
   // Returns true or false
   contains(value) {
-
+    const vertices = this.vertices;
+    for (let i = 0; i < vertices.length; i++) {
+      if (vertices[i].value === value) {
+        return true;
+      }
+    }
+    return false;
   }
   // Checks the graph to see if a GraphNode with the specified value exists in the graph 
   // and removes the vertex if it is found
   // This function should also handle the removing of all edge references for the removed vertex
   removeVertex(value) {
-
+    // const allvertices = this.vertices;
+    // for (let i = 0; i < allvertices.length; i++) {
+    //   if (allvertices[i].value === value) {
+    //     allvertices.splice(i);
+    //   }
   }
   // Checks the two input vertices to see if each one references the other in their respective edges array
   // Both vertices must reference each other for the edge to be considered valid
@@ -59,12 +83,25 @@ class Graph {
   // Note: You'll need to store references to each vertex's array of edges so that you can use 
   // array methods on said arrays. There is no method to traverse the edge arrays built into the GraphNode class
   checkIfEdgeExists(fromVertex, toVertex) {
-
+    let vertices = this.vertices;
+    vertices = vertices.filter((element) => {
+      return element.value === fromVertex;
+    });
+    for (let i = 0; i < vertices.length; i++) {
+      if (vertices[i].edges.includes(toVertex)) return true;
+    }        
+    return false;
   }
   // Adds an edge between the two given vertices if no edge already exists between them
   // Again, an edge means both vertices reference the other 
   addEdge(fromVertex, toVertex) {
-
+    let vertices = this.vertices;
+    vertices = vertices.filter((element) => {
+      return element.value === fromVertex;
+    });
+    for (let i = 0; i < vertices.length; i++) {
+      vertices[i].edges = toVertex;
+    }  
   }
   // Removes the edge between the two given vertices if an edge already exists between them
   // After removing the edge, neither vertex should be referencing the other
